0c9c4412463d91363f441f7840936228f9fe1d21
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-04-01 17:52:24 +0100

    Added ball with basic physics

--- jsPong.js
+++ jsPong.js
@@ -19,6 +19,27 @@ class Paddle {
     }
 }
 
+class Ball {
+    constructor(x, y, radius, colour) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.colour = colour;
+        this.velocity = [3, 3]; // Velocity x & y components
+    }
+    getSpeed(){
+        return Math.sqrt(this.velocity[0] ** 2 + this.velocity[1]**2);
+    }
+    move(){
+        this.x += this.velocity[0];
+        this.y += this.velocity[1];
+        if (this.y >= canvasHeight - this.radius || this.y - this.radius <= 0) {this.velocity[1] = -this.velocity[1];}
+    }
+    draw() {
+        createCircle(this.x, this.y, this.radius, this.colour);
+    }
+}
+
 function updatePoints(paddle) {
     createText(((canvasWidth / 2) + paddle.x) / 2, 20, paddle.points, "black", "20px Arial");
 }
@@ -26,6 +47,7 @@ function updatePoints(paddle) {
 // Initialise game components
 var userPaddle = new Paddle(10, 10, 20, 100);
 var compPaddle = new Paddle(canvasWidth - 30, 10, 20, 100);
+var ball = new Ball(canvasWidth/2, canvasHeight/2, 5,"black");
 
 function createRectangle(x, y, width, height, colour) {
     context.fillStyle = colour;
@@ -51,6 +73,10 @@ function clearCanvas(){
 }
 
 function render(){
+    console.log("render");
+    clearCanvas();
+    ball.move();
+    ball.draw();
     userPaddle.draw();
     compPaddle.draw();
     updatePoints(userPaddle);
@@ -59,7 +85,6 @@ function render(){
 
 document.addEventListener("keydown", event => {
     console.log(event.code);
-    clearCanvas();
     if (event.code === "KeyS"){userPaddle.y++;}
     if (event.code === "ArrowDown"){compPaddle.y++;}
 
@@ -69,4 +94,6 @@ document.addEventListener("keydown", event => {
     render();
 });
 
-render();
+const fps = 60;
+
+setInterval(render, 1000/fps);
