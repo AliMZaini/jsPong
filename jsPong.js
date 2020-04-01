0259dcf11bdea5d7e2a2d6e15438bbdd5f85c9f8
Ali Z <51179189+AliMZaini@users.noreply.github.com>
2020-04-01 20:33:38 +0100

    Increased paddle speed

--- jsPong.js
+++ jsPong.js
@@ -3,6 +3,8 @@ const context = canvas.getContext("2d");
 const canvasWidth = document.getElementById("pongCanvas").getAttribute('width');
 const canvasHeight = document.getElementById("pongCanvas").getAttribute('height');
 
+const PADDLE_SPEED = 7;
+const FPS = 60;
 const BACKGROUND_COLOUR = "#9dc6a7";
 
 class Paddle {
@@ -15,6 +17,7 @@ class Paddle {
         this.vertical = vertical;
         this.points = 0;
     }
+
     draw() {
         // TODO pass by reference
         if (this.vertical) {
@@ -24,7 +27,7 @@ class Paddle {
             if (this.y <= 0) {
                 this.y = 0;
             }
-        }else {
+        } else {
             if (this.x >= canvasWidth - this.width) {
                 this.x = canvasWidth - this.width;
             }
@@ -34,6 +37,7 @@ class Paddle {
         }
         createRectangle(this.x, this.y, this.width, this.height, this.colour);
     }
+
     shorten(length) {
         // TODO pass by reference
         if (this.vertical) {
@@ -41,7 +45,7 @@ class Paddle {
                 this.height -= length;
                 this.y += length / 2;
             }
-        }else {
+        } else {
             if (this.width - length > 0) {
                 this.width -= length;
                 this.x += length / 2;
@@ -58,13 +62,14 @@ class Ball {
         this.colour = colour;
         this.velocity = velocity; // velocity is an array with an x & y component
     }
-    move(){
+
+    move() {
         this.x += this.velocity[0];
         this.y += this.velocity[1];
         //if (this.y >= canvasHeight - this.radius || this.y - this.radius <= 0) {this.velocity[1] = -this.velocity[1];} // this would make the ball bounce off the horizontal walls
 
-        for (let paddle of paddles){
-            if (checkCollision(paddle, this)){
+        for (let paddle of paddles) {
+            if (checkCollision(paddle, this)) {
                 paddle.points++;
                 paddle.shorten(5);
                 this.velocity[0] = -this.velocity[0];
@@ -73,12 +78,13 @@ class Ball {
             }
         }
 
-        if (this.x > canvasWidth || this.x < 0){
+        if (this.x > canvasWidth || this.x < 0) {
             console.log("ball is out of canvas");
-            this.x = canvasWidth/2;
-            this.y = canvasHeight/2;
+            this.x = canvasWidth / 2;
+            this.y = canvasHeight / 2;
         }
     }
+
     draw() {
         createCircle(this.x, this.y, this.radius, this.colour);
     }
@@ -86,15 +92,15 @@ class Ball {
 
 // Initialise game components
 // TODO sizes should depend on the canvas size
-var leftPaddle = new Paddle(10, canvasHeight/2 - 50, 20, 100, "#a8e6cf", true);
-var rightPaddle = new Paddle(canvasWidth - 30, canvasHeight/2 - 50, 20, 100, "#ccedd2", true);
-var topPaddle = new Paddle(canvasWidth/2 - 50, 10, 100, 20,"#effcef", false);
-var bottomPaddle = new Paddle(canvasWidth/2 - 50, canvasHeight - 30, 100, 20, "#d1f5d3", false);
+var leftPaddle = new Paddle(10, canvasHeight / 2 - 50, 20, 100, "#a8e6cf", true);
+var rightPaddle = new Paddle(canvasWidth - 30, canvasHeight / 2 - 50, 20, 100, "#ccedd2", true);
+var topPaddle = new Paddle(canvasWidth / 2 - 50, 10, 100, 20, "#effcef", false);
+var bottomPaddle = new Paddle(canvasWidth / 2 - 50, canvasHeight - 30, 100, 20, "#d1f5d3", false);
 
 var paddles = [leftPaddle, rightPaddle, topPaddle, bottomPaddle];
 
-var ballOne = new Ball(canvasWidth/2, canvasHeight/2, 5,[-3, -2], "blue");
-var ballTwo = new Ball(canvasWidth/2, canvasHeight/2, 5, [3, 2], "red");
+var ballOne = new Ball(canvasWidth / 2, canvasHeight / 2, 5, [-3, -2], "blue");
+var ballTwo = new Ball(canvasWidth / 2, canvasHeight / 2, 5, [3, 2], "red");
 
 var balls = [ballOne, ballTwo];
 
@@ -117,7 +123,7 @@ function createText(x, y, text, colour, style) {
     context.fillText(text, x, y);
 }
 
-function clearCanvas(){
+function clearCanvas() {
     createRectangle(0, 0, canvasWidth, canvasHeight, BACKGROUND_COLOUR);
 }
 
@@ -127,7 +133,7 @@ function clearCanvas(){
  * @param ball object
  * @returns boolean if the ball and paddle have collided
  */
-function checkCollision(paddle, ball){
+function checkCollision(paddle, ball) {
     /**
      left of paddle must not touch right of ball
      right of paddle must not touch left of ball
@@ -144,16 +150,16 @@ function checkCollision(paddle, ball){
 }
 
 function updatePoints(paddle) {
-    createText((2 * paddle.x + paddle.width)/2 - 5, (2 * paddle.y + paddle.height)/2 + 5, paddle.points, "black", "20px Segoe UI");
+    createText((2 * paddle.x + paddle.width) / 2 - 5, (2 * paddle.y + paddle.height) / 2 + 5, paddle.points, "black", "20px Segoe UI");
 }
 
-function render(){
+function render() {
     clearCanvas();
-    for (let ball of balls){
+    for (let ball of balls) {
         ball.move();
         ball.draw();
     }
-    for (let paddle of paddles){
+    for (let paddle of paddles) {
         paddle.draw();
         updatePoints(paddle);
     }
@@ -162,20 +168,33 @@ function render(){
 // TODO change listener
 document.addEventListener("keydown", event => {
     //console.log(event.code);
-    if (event.code === "KeyS"){leftPaddle.y++;}
-    if (event.code === "ArrowDown"){rightPaddle.y++;}
+    if (event.code === "KeyS") {
+        leftPaddle.y += PADDLE_SPEED;
+    }
+    if (event.code === "ArrowDown") {
+        rightPaddle.y += PADDLE_SPEED;
+    }
 
-    if (event.code === "KeyW"){leftPaddle.y--;}
-    if (event.code === "ArrowUp"){rightPaddle.y--;}
+    if (event.code === "KeyW") {
+        leftPaddle.y -= PADDLE_SPEED;
+    }
+    if (event.code === "ArrowUp") {
+        rightPaddle.y -= PADDLE_SPEED;
+    }
 
-    if (event.code === "KeyD"){topPaddle.x++;}
-    if (event.code === "ArrowRight"){bottomPaddle.x++;}
+    if (event.code === "KeyD") {
+        topPaddle.x += PADDLE_SPEED;
+    }
+    if (event.code === "ArrowRight") {
+        bottomPaddle.x += PADDLE_SPEED;
+    }
 
-    if (event.code === "KeyA"){topPaddle.x--;}
-    if (event.code === "ArrowLeft"){bottomPaddle.x--;}
-
-    render();
+    if (event.code === "KeyA") {
+        topPaddle.x -= PADDLE_SPEED;
+    }
+    if (event.code === "ArrowLeft") {
+        bottomPaddle.x -= PADDLE_SPEED;
+    }
 });
 
-const fps = 60;
-setInterval(render, 1000/fps);
+setInterval(render, 1000 / FPS);
